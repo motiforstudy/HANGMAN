6a18ae60a0c4db9906e4ec7503ceef0b86b1c32a
moti jusewitz <motiforstudy123@gmail.com>
2025-10-27 09:50:24 +0200

    adding the choose a secret word function

---
+++ hangman/choose_word.py
@@ -0,0 +1,11 @@
+from random import randint
+from data.words import words
+
+
+def choose_secret_word (words : list[str]) -> str:
+
+    len_of_index = len(words)
+    random_number = randint(0, len_of_index)
+    secret_word = words[random_number]
+
+    return secret_word
---
+++ main.py
@@ -0,0 +1,8 @@
+from hangman.choose_word import choose_secret_word
+
+
+# def play (words:)
+
+
+
+
